04abba83bcbcf0215219e389dd497977b2a89d5e
Kenrick Guie <nyhunter77@yahoo.com>
2016-04-27 17:23:04 -0400

    first commit

---
+++ src/utils/api.jsx
@@ -0,0 +1,19 @@
+var Fetch = require('whatwg-fetch');
+var rootUrl = 'https://api.imgur.com/3/';
+var apiKey = 'fd027cf7096db89';
+
+module.exports = window.api = {
+	get: function(url) {
+		return fetch(rootUrl + url, {
+			headers: {
+				'Authorization': 'Client-ID ' + apiKey
+			}
+		})
+		.then(function(response) {
+			return response.json()
+		})
+		.then(function(data){
+			console.log(data);
+		})
+	}
+}
